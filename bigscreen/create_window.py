c66ad6e129355df4ba1885efbf37c19e01a9ee00
Nazar Zozulya <likeemangames@gmail.com>
2023-11-25 21:50:38 +0300

    Добавили боковое менюэ

--- bigscreen/create_window.py
+++ bigscreen/create_window.py
@@ -18,7 +18,7 @@ class App(tk.CTk):
                                 bg_color="#096C82",
                                 corner_radius=0,                              
                                 )
-        self.side_frame.grid(row = 275, column = 800,)
+        self.side_frame.grid(row = 1000, column = 800, )
 
         self.border_frame = tk.CTkFrame(self, 
                                 width =2, 
@@ -28,20 +28,46 @@ class App(tk.CTk):
                                 corner_radius=0,
                                 #    border_color='white',
                                     )
-        self.border_frame.grid(row = 275, column = 1800,padx=1,)
+        self.border_frame.grid(row = 1000, column = 1800,padx=1,)
 
-        self.pointed_minidate = tk.CTkFrame(self.side_frame,
-                                            width = 240,
-                                            height = 300,
-                                            fg_color = "red",
+        self.pointed_minidate = tk.CTkButton(self.side_frame,
+                                            text=' ',
+                                            width = 236,
+                                            height = 101,
+                                            hover_color='#4599A4',
+                                            fg_color = "#4599A4",
                                             border_color = 'white',
                                             border_width=2.5,
                                             corner_radius=20
                                             )
-        self.pointed_minidate.grid(row = 240, column = 1000, padx = 23, pady = 22,)
+        self.pointed_minidate.grid(row = 0, column = 0, padx = 23, pady = 22)
 
-        self.title = tk.CTkLabel(self.pointed_minidate, text = "Дніпро", font = ("Inter", 58), fg_color = "transparent")
-        self.title.grid(row=0,column=0, padx=0, pady=50)
+        self.title = tk.CTkLabel(self.pointed_minidate, text = "Поточна позиція", font = ("Roboto Slab", 18), )
+        self.title.grid(row=0,column=0, padx=10, pady=8,sticky='nw')
+
+        self.city = tk.CTkLabel(self.pointed_minidate, text = "Дніпро", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        self.city.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
+
+        self.temp = tk.CTkLabel(self.pointed_minidate, text = "11°", font = ("Roboto Slab", 10),bg_color='red')
+        self.temp.grid(row=0,column=0, padx=170, pady=10,sticky='ne')
+
+        # self.pointed_minidate = tk.CTkButton(self.side_frame,
+        #                                     text=' ',
+        #                                     width = 236,
+        #                                     height = 101,
+        #                                     hover_color='#096C82',
+        #                                     fg_color = "#096C82",
+        #                                     border_color = 'white',
+        #                                     border_width=2.5,
+        #                                     corner_radius=20
+        #                                     )
+        # self.pointed_minidate.grid(row = 0, column = 0, padx = 23, pady = 22,)
+
+        # self.title = tk.CTkLabel(self.pointed_minidate, text = "Поточна позиція", font = ("Roboto Slab", 18), )
+        # self.title.grid(row=0,column=0, padx=10, pady=8,sticky='nw')
+
+        # self.city = tk.CTkLabel(self.pointed_minidate, text = "Дніпро", font = ("Roboto Slab", 12), bg_color = "transparent", height=7)
+        # self.city.grid(row=0,column=0, padx=10, pady=30,sticky='nw')
 
         # self.city = tk.CTkTextbox(master =self.pointed_minidate,
         #                         #  text="City",
