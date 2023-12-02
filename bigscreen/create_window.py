bb1a279f19f6c8484c89aa6dd3fe2c5a29122fb1
Nazar Zozulya <likeemangames@gmail.com>
2023-12-02 21:43:58 +0300

    Добавили базу данных

--- bigscreen/create_window.py
+++ bigscreen/create_window.py
@@ -1,8 +1,6 @@
 # import tkinter
 import customtkinter as tk
 
-
-
 class App(tk.CTk):
     def __init__(self):
         super().__init__()
@@ -11,14 +9,14 @@ class App(tk.CTk):
         self.resizable(False, False)
         self.config(bg="#5DA7B1",)
 
-        self.side_frame = tk.CTkScrollableFrame(self, 
+        self.side_frame = tk.CTkFrame(self, 
                                 width = 275, 
                                 height = 800, 
                                 fg_color = "#096C82", 
                                 bg_color="#096C82",
                                 corner_radius=0,                              
                                 )
-        self.side_frame.grid(row = 1000, column = 800, )
+        self.side_frame.grid(row = 0, column = 0,padx=0,pady=0, sticky='wn')
 
         self.border_frame = tk.CTkFrame(self, 
                                 width =2, 
@@ -28,65 +26,218 @@ class App(tk.CTk):
                                 corner_radius=0,
                                 #    border_color='white',
                                     )
-        self.border_frame.grid(row = 1000, column = 1800,padx=1,)
+        self.border_frame.grid(row = 0, column = 1,padx=1,sticky='n')
+
+        # self.pointed_minidate = tk.CTkButton(self.side_frame,
+        #                                     text=' ',
+        #                                     width = 236,
+        #                                     height = 101,
+        #                                     hover_color='#4599A4',
+        #                                     fg_color = "#4599A4",
+        #                                     border_color = 'white',
+        #                                     border_width=2.5,
+        #                                     corner_radius=20
+        #                                     )
+        # self.pointed_minidate.grid(row = 0, column = 0, padx = 10, pady = (10,0))
+
+        # self.title = tk.CTkLabel(self.pointed_minidate, text = "Поточна позиція", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
+        # self.title.grid(row=0,column=0, padx=5, pady=10,sticky='nw')
+
+        # self.city = tk.CTkLabel(self.pointed_minidate, text = "Дніпро", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        # self.city.grid(row=0,column=0, padx=5, pady=31,sticky='nw')
+
+        # self.temp = tk.CTkLabel(self.pointed_minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        # self.temp.grid(row=0,column=3, ipadx=0, pady=3,sticky='ne')
+
+        # self.text_temp = tk.CTkLabel(self.pointed_minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        # self.text_temp.grid(row=0,column=0, padx=6, pady=(60,0),sticky='nw')
+
+        # self.text_temp = tk.CTkLabel(self.pointed_minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        # self.text_temp.grid(row=0,column=3, ipadx=0, pady=(60,0),sticky='ne')
+
+
 
         self.pointed_minidate = tk.CTkButton(self.side_frame,
-                                            text=' ',
-                                            width = 236,
-                                            height = 101,
-                                            hover_color='#4599A4',
-                                            fg_color = "#4599A4",
-                                            border_color = 'white',
-                                            border_width=2.5,
-                                            corner_radius=20
-                                            )
-        self.pointed_minidate.grid(row = 0, column = 0, padx = 10, pady = 22)
+                                    text=' ',
+                                    width = 236,
+                                    height = 101,
+                                    hover_color='#4599A4',
+                                    fg_color = "#4599A4",
+                                    border_color = 'white',
+                                    border_width=2.5,
+                                    corner_radius=20
+                                    )
+        self.pointed_minidate.grid(row = 0, column = 0, padx = 10, pady = 10,sticky='n')
 
-        self.title = tk.CTkLabel(self.pointed_minidate, text = "Поточна позиція", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
-        self.title.grid(row=0,column=0, padx=5, pady=10,sticky='nw')
-
-        self.city = tk.CTkLabel(self.pointed_minidate, text = "Дніпро", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
-        self.city.grid(row=0,column=0, padx=5, pady=31,sticky='nw')
-
-        self.temp = tk.CTkLabel(self.pointed_minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
-        self.temp.grid(row=0,column=3, ipadx=0, pady=3,sticky='ne')
-
-        self.text_temp = tk.CTkLabel(self.pointed_minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
-        self.text_temp.grid(row=0,column=0, padx=6, pady=(60,0),sticky='nw')
-
-        self.text_temp = tk.CTkLabel(self.pointed_minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
-        self.text_temp.grid(row=0,column=3, ipadx=0, pady=(60,0),sticky='ne')
-
-
-
-        # self.pointed_minidate1 = tk.CTkButton(self.side_frame,
-        #                             text=' ',
-        #                             width = 236,
-        #                             height = 101,
-        #                             hover_color='#4599A4',
-        #                             fg_color = "#4599A4",
-        #                             border_color = 'white',
-        #                             border_width=2.5,
-        #                             corner_radius=20
-        #                             )
-        # self.pointed_minidate1.grid(row = 0, column = 0, padx = 20, pady = 22+50)
-
-        # self.title1 = tk.CTkLabel(self.pointed_minidate1, text = "Поточна позиція", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
-        # self.title1.grid(row=0,column=0, padx=10, pady=10,sticky='nw')
+        self.title1 = tk.CTkLabel(self.pointed_minidate, text = "Поточна позиція", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
+        self.title1.grid(row=0,column=0, padx=10, pady=10,sticky='nw')
                         
-        # self.city1 = tk.CTkLabel(self.pointed_minidate1, text = "Дніпро", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
-        # self.city1.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
+        self.city1 = tk.CTkLabel(self.pointed_minidate, text = "Дніпро", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        self.city1.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
 
-        # self.temp1 = tk.CTkLabel(self.pointed_minidate1, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
-        # self.temp1.grid(row=0,column=3, ipadx=0, pady=3,sticky='ne')
+        self.temp1 = tk.CTkLabel(self.pointed_minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.temp1.grid(row=0,column=3, padx=0, pady=3,sticky='ne')
 
-        # self.text_temp1 = tk.CTkLabel(self.pointed_minidate1, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
-        # self.text_temp1.grid(row=0,column=0, padx=10, pady=(60,0),sticky='nw')
+        self.text_temp1 = tk.CTkLabel(self.pointed_minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,column=0, padx=10, pady=(60,0),sticky='nw')
 
-        # self.text_temp1 = tk.CTkLabel(self.pointed_minidate1, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
-        # self.text_temp1.grid(row=0,column=3, ipadx=0, pady=(60,0),sticky='ne')
+        self.text_temp1 = tk.CTkLabel(self.pointed_minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,column=3, ipadx=0, pady=(60,0),sticky='ne')
 
 
+
+        self.minidate = tk.CTkButton(self.side_frame,
+                                    text=' ',
+                                    width = 270,
+                                    height = 101,
+                                    hover_color='#096C82',
+                                    fg_color = "transparent",
+                                    border_color = 'white',
+                                    border_width=2.5,
+                                    corner_radius=20
+                                    )
+        self.minidate.grid(row = 2, column = 0, padx = 10, pady = 14,sticky='n')
+
+        self.title1 = tk.CTkLabel(self.minidate, text = "Київ", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
+        self.title1.grid(row=0,column=0, padx=10, pady=10,sticky='nw')
+                        
+        self.city1 = tk.CTkLabel(self.minidate, text = "14:37", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        self.city1.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
+
+        self.temp1 = tk.CTkLabel(self.minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.temp1.grid(row=0,columnspan=10,column=1, padx=10, pady=4,sticky='ne')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,column=0, padx=10, pady=(60,0),sticky='nw')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,columnspan=10, column=1,padx=10, pady=(60,0),sticky='ne')
+
+        self.minidate = tk.CTkButton(self.side_frame,
+                                    text=' ',
+                                    width = 270,
+                                    height = 101,
+                                    hover_color='#096C82',
+                                    fg_color = "transparent",
+                                    border_color = 'white',
+                                    border_width=2.5,
+                                    corner_radius=20
+                                    )
+        self.minidate.grid(row = 3, column = 0, padx = 10, pady = 14,sticky='n')
+
+        self.title1 = tk.CTkLabel(self.minidate, text = "Рим", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
+        self.title1.grid(row=0,column=0, padx=10, pady=10,sticky='nw')
+                        
+        self.city1 = tk.CTkLabel(self.minidate, text = "13:37", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        self.city1.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
+
+        self.temp1 = tk.CTkLabel(self.minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.temp1.grid(row=0,columnspan=10,column=1, padx=10, pady=4,sticky='ne')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,column=0, padx=10, pady=(60,0),sticky='nw')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,columnspan=10, column=1,padx=10, pady=(60,0),sticky='ne')
+
+        self.minidate = tk.CTkButton(self.side_frame,
+                                    text=' ',
+                                    width = 270,
+                                    height = 101,
+                                    hover_color='#096C82',
+                                    fg_color = "transparent",
+                                    border_color = 'white',
+                                    border_width=2.5,
+                                    corner_radius=20
+                                    )
+        self.minidate.grid(row = 4, column = 0, padx = 10, pady = 14,sticky='n')
+
+        self.title1 = tk.CTkLabel(self.minidate, text = "Лондон", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
+        self.title1.grid(row=0,column=0, padx=10, pady=10,sticky='nw')
+                        
+        self.city1 = tk.CTkLabel(self.minidate, text = "12:37", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        self.city1.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
+
+        self.temp1 = tk.CTkLabel(self.minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.temp1.grid(row=0,columnspan=10,column=1, padx=10, pady=4,sticky='ne')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,column=0, padx=10, pady=(60,0),sticky='nw')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,columnspan=10, column=1,padx=10, pady=(60,0),sticky='ne')
+
+        self.minidate = tk.CTkButton(self.side_frame,
+                                    text=' ',
+                                    width = 270,
+                                    height = 101,
+                                    hover_color='#096C82',
+                                    fg_color = "transparent",
+                                    border_color = 'white',
+                                    border_width=2.5,
+                                    corner_radius=20
+                                    )
+        self.minidate.grid(row = 5, column = 0, padx = 10, pady = 14,sticky='n')
+
+        self.title1 = tk.CTkLabel(self.minidate, text = "Варшава", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
+        self.title1.grid(row=0,column=0, padx=10, pady=10,sticky='nw')
+                        
+        self.city1 = tk.CTkLabel(self.minidate, text = "13:37", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        self.city1.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
+
+        self.temp1 = tk.CTkLabel(self.minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.temp1.grid(row=0,columnspan=10,column=1, padx=10, pady=4,sticky='ne')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,column=0, padx=10, pady=(60,0),sticky='nw')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,columnspan=10, column=1,padx=10, pady=(60,0),sticky='ne')
+
+        self.minidate = tk.CTkButton(self.side_frame,
+                                    text=' ',
+                                    width = 270,
+                                    height = 101,
+                                    hover_color='#096C82',
+                                    fg_color = "transparent",
+                                    border_color = 'white',
+                                    border_width=2.5,
+                                    corner_radius=20
+                                    )
+        self.minidate.grid(row = 6, column = 0, padx = 10, pady = 14,sticky='n')
+
+        self.title1 = tk.CTkLabel(self.minidate, text = "Прага", font = ("Roboto Slab", 18), height=0,width=0,fg_color="transparent")
+        self.title1.grid(row=0,column=0, padx=10, pady=10,sticky='nw')
+                        
+        self.city1 = tk.CTkLabel(self.minidate, text = "13:37", font = ("Roboto Slab", 12), bg_color = "transparent", height=1)
+        self.city1.grid(row=0,column=0, padx=10, pady=31,sticky='nw')
+
+        self.temp1 = tk.CTkLabel(self.minidate, text = "11°", font = ("Roboto Slab", 50),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.temp1.grid(row=0,columnspan=10,column=1, padx=10, pady=4,sticky='ne')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "Хмарно з \nпроясненнями", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,column=0, padx=10, pady=(60,0),sticky='nw')
+
+        self.text_temp1 = tk.CTkLabel(self.minidate, text = "\nмакс.: 12 , мін.: 5  ", font = ("Roboto Slab", 12),bg_color='transparent',fg_color='transparent',height=1,width=1)
+        self.text_temp1.grid(row=0,columnspan=10, column=1,padx=10, pady=(60,0),sticky='ne')
+
+
+
+
+
+
+
+
+
+
+        self.content_up = tk.CTkFrame(self,width=900,height=100,fg_color='red')
+        self.content_up.grid(row=0,column=2,sticky='ne',padx=10,pady=10)
+        self.content_middle = tk.CTkFrame(self,width=900,height=300,fg_color='blue')
+        self.content_middle.grid(row=0,column=2,sticky='ne',padx=10,pady=120)
+        self.content_bottom = tk.CTkFrame(self,width=900,height=250,fg_color='green')
+        self.content_bottom.grid(row=0,column=2,sticky='ne',padx=10,pady=430)
+
+        
 app = App()
 
 app.mainloop()
--- registration/registration_window.py
+++ registration/registration_window.py
@@ -2,30 +2,35 @@
 import customtkinter as tk
 import requests
 import json
-
+import sqlite3 
+import os
 
 # city = input("Enter the city name: ")
 # url_api = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_key}"
+# path = os.path.abspath(__file__+"/../../user_data")
+# os.chdir(path)
 
 
+db = sqlite3.connect('user_data.db')
 
-# class Fields(tk.CTk):
-#     def __init__(self):
-#         super().__init__()
+pen = db.cursor()
 
-#         self.city_text = tk.CTkLabel(self.fields, text = "Країна:", font = ("Roboto Slab", 22))
-#         self.city_text.grid(row=0, column=0,padx=14,pady=10,sticky='wn')
+# sql.execute("""CREATE TABLE IF NOT EXISTS users (
+#         country TEXT,
+#         city TEXT,
+#         name TEXT,
+#         surname TEXT
+# )""")
 
-#         self.city_field = tk.CTkTextbox(self.fields,
-#                                 width = 218,
-#                                 height = 46, 
-#                                 fg_color = "#096C82",
-#                                 # bg_color = "#096C82",
-#                                 corner_radius = 15,
-#                                 border_width = 2,
-#                                 border_color = "white"
-#                                 )
-#         self.city_field.grid(row=0, column=0,padx=8,pady=44,sticky='wn')
+# sql.execute("INSERT INTO users VALUES('Ukraine', 'Dnipro', 'Nazar', 'Zozulya')")
+
+# pen.execute("SELECT rowid, * FROM users")
+# print(pen.fetchall())
+
+# items = pen.fetchall()
+
+# for el in items:
+    # print(el[1])
 
 
 
@@ -49,22 +54,20 @@ class Register(tk.CTk):
 
 
 
-        self.city_text = tk.CTkLabel(self.fields, text = "Країна:", font = ("Roboto Slab", 22))
-        self.city_text.grid(row=0, column=0,padx=14,pady=10,sticky='wn')
+        self.country_text = tk.CTkLabel(self.fields, text = "Країна:", font = ("Roboto Slab", 22))
+        self.country_text.grid(row=0, column=0,padx=14,pady=10,sticky='wn')
 
-        self.city_field = tk.CTkTextbox(self.fields,
+        self.country_field = tk.CTkTextbox(self.fields,
                                 width = 218,
                                 height = 46, 
                                 fg_color = "#096C82",
                                 # bg_color = "#096C82",
                                 corner_radius = 15,
                                 border_width = 2,
-                                border_color = "white"
+                                border_color = "white",
                                 )
-        self.city_field.grid(row=0, column=0,padx=8,pady=44,sticky='wn')
-
-        # Fields()
-
+        
+        self.country_field.grid(row=0, column=0,padx=8,pady=44,sticky='wn')
 
 
 
@@ -79,11 +82,31 @@ class Register(tk.CTk):
                                 # bg_color = "#096C82",
                                 corner_radius = 15,
                                 border_width = 2,
-                                border_color = "white"
+                                border_color = "white",
                                 )
         self.city_field.grid(row=0, column=0,padx=8,pady=44+90,sticky='wn')
 
+        def inputs():
 
+            pen.execute(f"INSERT INTO users VALUES('{self.country_field.get("1.0")}', '{self.city_field}', 'Nazar', 'Zozulya')")
+
+            pen.execute("SELECT rowid, * FROM users")
+            # print(pen.fetchall())
+            print(self.country_field)
+            print(pen.fetchall())
+            
+       
+
+
+            db.commit()
+            db.close()
+            # items = pen.fetchall()
+
+            # for el in items:
+            #     print(el[1])
+            # print(self.city_field["text"])
+            # print(1231241251236367547)
+    
 
 
         self.city_text = tk.CTkLabel(self.fields, text = "Ім'я:", font = ("Roboto Slab", 22))
@@ -116,6 +139,23 @@ class Register(tk.CTk):
                                 )
         self.city_field.grid(row=0, column=0,padx=8,pady=44+270,sticky='wn')
         
+        # self.reg_button = tk.CTkButton(self, 
+        #                                 text='Зберегти',
+        #                                 width=218,
+        #                                 height=46,
+        #                                 fg_color='red',
+        #                                 corner_radius=20,
+        #                                 border_color='white',
+        #                                 border_width=3,
+        #                                 )
+        # self.reg_button.grid(row=0, column=10,padx=0,pady=(0,10),sticky='s')
+        self.save_button = tk.CTkButton(self, fg_color = "#096C82", width = 218, height = 46, text='Зберегти', hover_color='#074A59',bg_color='#5DA7B1',border_color='white',border_width=2,corner_radius=15, font = ("Roboto Slab", 18), command=inputs)
+        self.save_button.grid(row=0, column=0, padx=0, pady=(320,0), )
+
+
+        
+            
+            
 
 register = Register()
 # API_key = "58cd4aa232816cd0ae10351cb15604aa"
